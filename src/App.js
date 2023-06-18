06a7abb379616551e56e9f58d5215f1c4bdcfd59
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2023-06-17 20:29:19 -0400

    added d3 and react version circle comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import Svg from './components/Svg';
+import Circle from './components/Circle';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+      <Svg />
+      <Circle />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Circle_React_Version.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Circle_React_Version = () => {
+  return (
+		<svg>
+			<circle cx='150' cy='77' r='40' />
+		</svg>
+	);
+}
+
+export default Circle_React_Version
